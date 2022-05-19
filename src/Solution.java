10ca5f97da33d742ef3d768ff9a40edcac2f15db
Deadshut64 <sergeychuchmanov@gmail.com>
2022-05-19 16:48:25 +0400

    version1

---
+++ src/Solution.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        BufferedReader reader =  new BufferedReader(new InputStreamReader(System.in));
+        Man man = new Man("Sergey");
+        System.out.println(man.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
